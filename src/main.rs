ef8c50638c2877eabe658962b0f48dbc46855773
defi-season <frankli.defi@outlook.com>
2021-04-12 11:31:14 -0400

    add total supply api

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use dotenv::dotenv;
 use hex_literal::hex;
+use std::env;
 use web3::{
     contract::{Contract, Options},
-    types::{U256, H160},
+    types::{H160, U256},
 };
-use std::env;
-use dotenv::dotenv;
 
 async fn check(infura_key: &str) -> web3::contract::Result<U256> {
-    let transport = web3::transports::WebSocket::new(format!("wss://mainnet.infura.io/ws/v3/{}", infura_key).as_str()).await?;
+    let transport = web3::transports::WebSocket::new(
+        format!("wss://mainnet.infura.io/ws/v3/{}", infura_key).as_str(),
+    )
+    .await?;
     let web3 = web3::Web3::new(transport);
 
     // Accessing existing contract.
@@ -22,9 +25,9 @@ async fn check(infura_key: &str) -> web3::contract::Result<U256> {
 
     let accounts: Vec<H160> = vec![
         hex!("40DD33bBe1FCcE62AD73040Fa07F8b1037ca6C7c").into(),
-        hex!("f66005e3fd85d4e7e6293B96FCf9Eadd32c56Cd2").into(), 
-        hex!("A16aF2b4A072F10FC2FDf505a76a0Dc7868b82cD").into(), 
-        hex!("bC7175790c7EAf4A429Ae0d20a98862FC1F0352A").into(), 
+        hex!("f66005e3fd85d4e7e6293B96FCf9Eadd32c56Cd2").into(),
+        hex!("A16aF2b4A072F10FC2FDf505a76a0Dc7868b82cD").into(),
+        hex!("bC7175790c7EAf4A429Ae0d20a98862FC1F0352A").into(),
         hex!("EAe020457249D82488dCEB45eB8E76258C6B4d61").into(),
         hex!("09cBA546a88CF031fFd69a21565E41e9Ca69108b").into(),
     ];
@@ -41,9 +44,13 @@ async fn check(infura_key: &str) -> web3::contract::Result<U256> {
 
     println!("sum: {}", sum);
 
-    let circulating_supply = (total_supply - sum)/decimals;
+    let circulating_supply = (total_supply - sum) / decimals;
 
-    println!("Circulating Supply: {} => {}", (total_supply - sum), circulating_supply);
+    println!(
+        "Circulating Supply: {} => {}",
+        (total_supply - sum),
+        circulating_supply
+    );
 
     Ok(circulating_supply)
 }
@@ -52,18 +59,22 @@ async fn check(infura_key: &str) -> web3::contract::Result<U256> {
 async fn supply() -> impl Responder {
     match env::var("INFURA_KEY") {
         Err(_e) => HttpResponse::Ok().body("Invalid key"),
-        Ok(infura_key) => {
-            match check(&infura_key).await {
-                Err(_e) => HttpResponse::Ok().body("Something went wrong"),
-                Ok(result) => {
-                    HttpResponse::Ok()
-                        .content_type("application/json; charset=utf-8")
-                        .header("Access-Control-Allow-Origin", "*")
-                        .body(format!("{}", result))
-                }
-            }
-        }
+        Ok(infura_key) => match check(&infura_key).await {
+            Err(_e) => HttpResponse::Ok().body("Something went wrong"),
+            Ok(result) => HttpResponse::Ok()
+                .content_type("application/json; charset=utf-8")
+                .header("Access-Control-Allow-Origin", "*")
+                .body(format!("{}", result)),
+        },
     }
+}
+
+#[get("/total")]
+async fn total() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("application/json; charset=utf-8")
+        .header("Access-Control-Allow-Origin", "*")
+        .body("90000000")
 }
 
 #[get("/")]
@@ -74,12 +85,8 @@ async fn index() -> impl Responder {
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    HttpServer::new(|| {
-        App::new()
-            .service(supply)
-            .service(index)
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    HttpServer::new(|| App::new().service(supply).service(total).service(index))
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
 }
